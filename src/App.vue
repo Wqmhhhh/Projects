ee104317bc04fdfa9574e2c940906c90d3866b85
Wqmhhhh <3105324129@qq.com>
2025-03-13 16:34:14 +0800

    primary

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+</script>
 
 <template>
-  <div>hello</div>
+  <el-config-provider :locale="zhCn">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <style scoped></style>
