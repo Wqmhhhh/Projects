37d7fe0d3ac9784309c041deb5a4ca1b822d52b5
Wqmhhhh <3105324129@qq.com>
2025-03-13 13:31:02 +0800

    primary

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import pluginPrettier from 'eslint-plugin-prettier' // 新增
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ElMessage: 'readonly',
+        ElMessageBox: 'readonly',
+        ElLoading: 'readonly'
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    plugins: {
+      prettier: pluginPrettier // 注册 Prettier 插件
+    },
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true, // 单引号
+          semi: false, // 无分号
+          printWidth: 80, // 每行宽度至多80字符
+          trailingComma: 'none', // 不加对象|数组最后逗号
+          endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+        }
+      ],
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+      'no-undef': 'error'
+    }
+  }
+]
---
+++ src/utils/request.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+// 设置api的基础URL
+const baseURL = 'http://'
+
+// 创建 Axios 实例
+const instance = axios.create({
+  timeout: 10000, // 设置超时时间（毫秒）
+  baseURL,
+  // 设置请求头
+  headers: {}
+})
+
+// 导出实例
+export default instance
